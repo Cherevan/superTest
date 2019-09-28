8fd5ceaa70c816af3506425dc7e1a72629fc63d8
JIexa <jiexack@gmail.com>
2019-09-28 17:06:17 +0300

    'bob_football'

--- 01-Bob-and-football.js
+++ 01-Bob-and-football.js
@@ -1,21 +1,24 @@
 /* eslint-disable no-console */
-// function getRandomNumber(max) {
-//   return Math.floor(Math.random() * (max + 1));
-// }
+/* eslint linebreak-style: ["error", "windows"] */
 
-// function bobVisitPub(bob) {
-//   return bob + getRandomNumber(6);
-// }
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
-// function simulateMatch() {
-//   return {
-//     home: getRandomNumber(5),
-//     away: getRandomNumber(5),
-//   };
-// }
+function bobVisitPub(bob) {
+  return bob + getRandomNumber(6);
+}
 
-// const mrBob = 0;
-// const cyroz = 150;
+function simulateMatch() {
+  return {
+    home: getRandomNumber(5),
+    away: getRandomNumber(5),
+  };
+}
+
+let mrBob = 0;
+let season = 0;
+const cyroz = 350;
 const clubs = [
   'Arsenal',
   'Man United',
@@ -26,5 +29,54 @@ const clubs = [
   'Lester',
   'Chelsea',
 ];
+const table = {};
+for (let i = 0; i < clubs.length; i += 1) {
+  table[clubs[i]] = 0;
+}
+while (mrBob <= cyroz) {
+  for (let i = 0; i < clubs.length; i += 1) {
+    for (let j = 0; j < clubs.length; j += 1) {
+      if (i !== j) {
+        const play1 = simulateMatch();
+        if (play1.home > play1.away) {
+          table[clubs[i]] += 3;
+        } else if (play1.home < play1.away) {
+          table[clubs[j]] += 3;
+        } else {
+          table[clubs[j]] += 1;
+          table[clubs[i]] += 1;
+        }
 
-console.log('Clubs in League:', clubs.length);
+        const play2 = simulateMatch();
+        if (play2.home > play2.away) {
+          table[clubs[i]] += 3;
+        } else if (play2.home < play2.away) {
+          table[clubs[j]] += 3;
+        } else {
+          table[clubs[j]] += 1;
+          table[clubs[i]] += 1;
+        }
+        // Боб идет бухать:
+        if (play1.home + play1.away > 6) {
+          mrBob = bobVisitPub(mrBob);
+        }
+
+        if (play2.home + play2.away > 6) {
+          mrBob = bobVisitPub(mrBob);
+        }
+
+        if (mrBob >= cyroz) {
+          break;
+        }
+      }
+    }
+    if (mrBob >= cyroz) {
+      break;
+    }
+  }
+  season += 1;
+}
+
+console.log(table);
+console.log('Mr Bob vupil piva:', mrBob, 'litra');
+console.log('Mr Bob vuderzal', season, 'sezona');
